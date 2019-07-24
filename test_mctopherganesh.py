3a7220872831fa240971e640a84602a4f82386ff
Christopher Ganesh McMahon <mctopherganesh@gmail.com>
2019-07-24 18:41:24 -0500

    adds blog roll page, takes away .ipynb html from home page

---
+++ test_mctopherganesh.py
@@ -0,0 +1,7 @@
+import logging
+import datetime
+
+
+logging.basicConfig(filename='app.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
+logging.warning('done testing.'+ ', ' + str(datetime.datetime.now()))
