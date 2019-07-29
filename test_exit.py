685d9424565f0b922564d20181e1e89fcefc2c45
Christopher Ganesh McMahon <mctopherganesh@gmail.com>
2019-07-29 00:44:35 -0500

    adds day6.html page and another <tr> in blog.html

--- test_exit.py
+++ test_exit.py
@@ -3,10 +3,9 @@ from atexit import register
 def easy_funk():
     print('hello cruel world')
 
-
-
-
 test_decorator=register(easy_funk)
-
-
 print('test string')
+
+
+# this import prints something on end of program execution
+
--- test_mctopherganesh.py
+++ test_mctopherganesh.py
@@ -2,6 +2,6 @@ import logging
 import datetime
 
 
-logging.basicConfig(filename='app.log', filemode='w',
+logging.basicConfig (filename='app.log', filemode='w',
                     format='%(name)s - %(levelname)s - %(message)s')
 logging.warning('done testing.'+ ', ' + str(datetime.datetime.now()))
